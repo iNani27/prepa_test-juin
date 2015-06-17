0533fa6baf2513d8ebcb8b704e38d1afa02956a7
Stagiaire <Stagiaire@192.168.178.253>
2015-06-17 13:52:47 +0200

    liens selon perm

--- index.php
+++ index.php
@@ -15,23 +15,20 @@ if (isset($_POST['lelogin'])) {
     INNER JOIN droit d ON u.droit_id = d.id
     WHERE u.lelogin = '$lelogin' AND u.lemdp = '$lemdp';";
     $req = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
-    $recup_user = mysqli_fetch_assoc($mysqli);
+    $recup_user = mysqli_fetch_assoc($req);
 
-    /*Reprendre ici
-     * Si on récupère un utilisateur
-     * if($recup_user)
-     * 
-     * 
-     * 
-     */
+    // Vérifier si on a récupérer un utilisateur
+    if (mysqli_num_rows($recup_user)) {
+
 // Si l'utilisateur s'est bien connecté  
-    $_SESSION = $recup_user; // transformation des résultats de la requête en variable de session
-    $_SESSION['sid'] = session_id(); // récupération de la clef de session
-    /*var_dump($_SESSION);*/
-    //redirection vers la page d'accueil
-    /*header('Location : '.CHEMIN_RACINE."?hahaok");*/
-    //Pour éviter les doubles connexion par F5
-    header('Location : '.CHEMIN_RACINE);
+        $_SESSION = $recup_user; // transformation des résultats de la requête en variable de session
+        $_SESSION['sid'] = session_id(); // récupération de la clef de session
+        /* var_dump($_SESSION); */
+        //redirection vers la page d'accueil
+        /* header('Location : '.CHEMIN_RACINE."?hahaok"); */
+        //Pour éviter les doubles connexion par F5
+        header('Location : ' . CHEMIN_RACINE);
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -48,16 +45,39 @@ if (isset($_POST['lelogin'])) {
             <div id="haut"><h1>photos.be</h1></div>
             <div id="connect">
                 <?php
-                if(!isset($_SESSION['sid']) || $_SESSION['sid']!=session_id()){
-                ?>
-                <form action="" name="connection" method="post">
-                    <input type="text" name="lelogin" required />
-                    <input type="password" name="lemdp" required />
-                    <input type="submit" value="Connexion" />
-                </form>
-                <a href="mdp.php">Mot de passe oublié?</a>
-                <a href="inscription.php">Inscription</a>
-                <?php
+                // si on est pas/plus connecté 
+                if (!isset($_SESSION['sid']) || $_SESSION['sid'] != session_id()) {
+                    ?>
+                    <form action="" name="connection" method="post">
+                        <input type="text" name="lelogin" required />
+                        <input type="password" name="lemdp" required />
+                        <input type="submit" value="Connexion" />
+                    </form>
+                    <a href="mdp.php">Mot de passe oublié?</a>
+                    <a href="inscription.php">Inscription</a>
+                    <?php
+                    // sinon on est connecté
+                } else {
+                    //Texte d'accueil
+                    echo "<h3>Bonjour " . $_SESSION['lenom'] . "</h3>";
+                    echo "<p>Vous êtes connecté en tant que <span title='" . $SESSION['ladesc'] . "'>" . $SESSION['nom_perm'] . "</span></p>";
+                    echo "<h5><a href='deconnect.php'>Déconnexion</a></h5>";
+                    // liens selon perm
+                    switch ($_SESSION["laperm"]) {
+                        // admin
+                        case 0:
+                            echo "<a href='admin.php'>Administrer le site</a>";
+                            echo "<a href='membre.php'>Espace membres</a>";
+                            break;
+                        // modérateur
+                        case 1:
+                            echo "<a href='moder.php'>Modérer le site</a>";
+                            echo "<a href='membre.php'>Espace membres</a>";
+                            break;
+                        // utilisateur
+                        default:
+                            echo "<a href='membre.php'>Espace membres</a>";
+                    }
                 }
                 ?>
             </div>
